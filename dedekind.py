ddd579f23d58f15be871e41dfd8fc0985b0c75e4
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2019-05-18 00:34:25 +0200

    fixed bugs.

--- dedekind.py
+++ dedekind.py
@@ -1,10 +1,11 @@
 import numpy as np
 import scipy.signal as signal
-
+import numpy.fft as fft
 WMAT_CACHE = {}
 
 
-def linear_filter_matrix(nf, df, patch_c, patch_w, filter_factor, zero_flags, flags, cache = WMAT_CACHE):
+def linear_filter_matrix(nf, df, patch_c, patch_w, filter_factor, zero_flags,
+flags, cache = WMAT_CACHE, discrete_ft = False):
     '''
     Args:
         nf, number of frequencies.
@@ -15,24 +16,36 @@ def linear_filter_matrix(nf, df, patch_c, patch_w, filter_factor, zero_flags, fl
         patch_w,  a list of window widths for delay values (floats) to filter.
         zero_flags, bool, if true, set values in flagged frequency channels to zero, even if not filtering.
         cache, optional dictionary to store precomputed filter matrices. Default, use dictionary in this namespace.
+        discrete_ft, if True, generate matrix using discrete fourier transform from a diagonal in delay.
     Returns:
-        weight matrix (nfreq, nfreq)
+        weight matrix (nfreq, nfreq) -- a linear filter matrix that removes structures
     '''
     freqs = np.arange(-nf/2,nf/2) * df
-    wkey = (nf,freqs[1]-freqs[0],filter_factor,zero_flags)+tuple(np.where(flags)[0])\
+    wkey = (nf,freqs[1]-freqs[0],filter_factor,zero_flags,discrete_ft)+tuple(np.where(flags)[0])\
     + tuple(patch_c) + tuple(patch_w)
     if not wkey in WMAT_CACHE:
-        fx,fy=np.meshgrid(freqs,freqs)
-        filter_mat_inv = np.zeros((nf,nf),dtype=complex)
-        for cp,cw in zip(patch_c, patch_w):
-            filter_mat_inv += np.sinc(2.*(fx-fy) * patch_w).astype(complex) * np.exp(-2j*np.pi*(fx-fy) * patch_c)
-        filter_mat_inv = filter_mat_inv + np.identity(len(freqs))*filter_factor
-
+        if not discrete_ft:
+            fx,fy=np.meshgrid(freqs,freqs)
+            filter_mat_inv = np.zeros((nf,nf),dtype=complex)
+            for cp,cw in zip(patch_c, patch_w):
+                if filter_factor > 0.:
+                    filter_mat_inv += np.sinc(2.*(fx-fy) * patch_w).astype(complex) * np.exp(-2j*np.pi*(fx-fy) * patch_c) / filter_factor
+            filter_mat_inv = filter_mat_inv + np.identity(len(freqs))
+        else:
+            delays = fft.fftshift(fft.fftfreq(nf,df))
+            fmat_diag = np.ones(nf,dtype=complex) * filter_factor
+            for cp,cw in zip(patch_c,patch_w):
+                fmat_diag[np.abs(delays-cp)<=cw] += (1./2./cw)
+            filter_mat_inv = np.diag(fmat_diag)
+            #generate discrete FT matrix.
+            imag,jmat = np.meshgrid(np.arange(-nf/2,nf/2),np.arange(-nf/2,nf/2))
+            ftmat = np.exp(2j * np.pi * imat * jmat / nf)
+            filter_mat_inv = np.dot(ftmat, np.dot(filter_mat_inv, np.conj(ftmat.T)))
         if zero_flags:
             filter_mat_inv[:,flags]=0.
             filter_mat_inv[flags,:]=0.
 
-        filter_mat = np.linalg.pinv(filter_mat_inv)*filter_factor
+        filter_mat = np.linalg.pinv(filter_mat_inv)
         WMAT_CACHE[wkey]=filter_mat
 
     return WMAT_CACHE[wkey]
@@ -95,6 +108,7 @@ def dedekind(freqs,ydata,flags,filter_factor = 1e-6,patch_c = [0.], patch_w = [1
         output = fft.ifft(output)/taper
         x = freqs
     else:
-        x = np.arange(-nf/2,nf/2)/(freqs[1]-freqs[0])
+        x = np.arange(-nf/2,nf/2)/((freqs[1]-freqs[0]) * nf)
+        output = fft.fftshift(output)
     #print(output.shape)
-    return delays,output
+    return x,output
